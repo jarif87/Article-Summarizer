efd1d0137ec6ba5441b402733317c2888d633a19
Sadik Al Jarif <sadikaljarif05@gmail.com>
2024-07-05 19:54:10 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,28 +1,10 @@
-import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def index():
-    if request.method == "POST":
-        input_text = request.form['text']
-        summary = summarize(input_text)
-        return render_template("result.html", input_text=input_text, output_text=summary)
-    else:
-        return render_template("index.html")
-
-def summarize(article):
-    response = requests.post("https://jarif-summarization.hf.space/run/predict", json={
-        "data": [
-            article,
-        ]
-    }).json()
-    data = response["data"]
-    if data:
-        return data[0]  # Assuming the summarized data is returned as a list with one element
-    else:
-        return "Error: Unable to summarize the text."
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
